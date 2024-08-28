c602d5f43e3bc298ac58cc6b15c3dc7b32544979
Prescott Murphy <prurph@gmail.com>
2024-08-28 09:24:45 -0400

    Complete Exercise 11

--- src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -9,13 +9,32 @@ export const programModeEnumMap = {
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
 } as const;
 
-export type IndividualProgram = unknown;
+type ProgramModeEnumMap = typeof programModeEnumMap;
+
+// Alternate approach: pass union to indexed access type:
+type _IndividualProgram = ProgramModeEnumMap[
+  | "ONE_ON_ONE"
+  | "SELF_DIRECTED"
+  | "PLANNED_ONE_ON_ONE"
+  | "PLANNED_SELF_DIRECTED"];
+
+// Exclude approach:
+export type IndividualProgram = ProgramModeEnumMap[Exclude<
+  keyof ProgramModeEnumMap,
+  "GROUP" | "ANNOUNCEMENT"
+>];
 
 type tests = [
+  Expect<
+    Equal<
+      _IndividualProgram,
+      "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
+    >
+  >,
   Expect<
     Equal<
       IndividualProgram,
       "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
     >
-  >,
+  >
 ];
