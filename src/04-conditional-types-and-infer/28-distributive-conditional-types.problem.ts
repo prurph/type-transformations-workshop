c6cb0dc4515ffd02beea30b0787f4ea0c6d3299d
Prescott Murphy <prurph@gmail.com>
2024-08-30 11:06:09 -0400

    Complete Exercise 28

--- src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -2,6 +2,24 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Fruit = "apple" | "banana" | "orange";
 
-type AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
+// This doesn't work: compares the entire Fruit union against just apple | banana,
+// therefore Fruit does *not* extend apple | banana in this context and you get never.
+type _AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
 
-type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
+// Instead, using a generic context, you get "distributive conditinoal types", where
+// the condition is _applied to each member of the union_, not the entire union.
+type GetAppleBanana<T> = T extends "apple" | "banana" ? T : never;
+type AppleOrBanana = GetAppleBanana<Fruit>;
+
+// Alternate syntax: capture each member of Fruit using generic inline, then distributivity
+// applies to the condition.
+type AppleOrBananaInline = Fruit extends infer T
+  ? T extends "apple" | "banana"
+    ? T
+    : never
+  : never;
+
+type tests = [
+    Expect<Equal<AppleOrBanana, "apple" | "banana">>
+    Expect<Equal<AppleOrBananaInline, "apple" | "banana">>,
+];
